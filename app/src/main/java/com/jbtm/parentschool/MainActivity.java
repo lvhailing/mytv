19fe621f5b4d3c84109156bc6108f4c1d6bf440a
lvhailing <hailing.lv@ele.me>
2018-12-05 16:05:54 +0800

    test

--- app/src/main/java/com/jbtm/parentschool/MainActivity.java
+++ app/src/main/java/com/jbtm/parentschool/MainActivity.java
@@ -33,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(gridLayoutManager);
         recyclerView.setFocusable(false);
         border.attachTo(recyclerView);
-
     }
 
     private void initData() {
