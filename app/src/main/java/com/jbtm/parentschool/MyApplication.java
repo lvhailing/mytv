27a9f7b60d14a4f027d63056adc511d0a4c83124
lvhailing <hailing.lv@ele.me>
2019-03-11 09:41:24 +0800

    sdlzkndjif

--- app/src/main/java/com/jbtm/parentschool/MyApplication.java
+++ app/src/main/java/com/jbtm/parentschool/MyApplication.java
@@ -3,6 +3,7 @@ package com.jbtm.parentschool;
 import android.app.Application;
 
 import com.tencent.bugly.crashreport.CrashReport;
+import com.xiaomi.mistatistic.sdk.MiStatInterface;
 
 /**
  * Created by lvhailing on 2018/12/14.
@@ -17,11 +18,20 @@ public class MyApplication extends Application {
 
         instance = this;
 
+        //腾讯bugly
         initBugly();
+
+        //小米统计
+        initMiStat();
     }
 
-
+    //腾讯bugly
     private void initBugly() {
         CrashReport.initCrashReport(getApplicationContext(), "024cdf058a", false);
+    }
+
+    //小米统计
+    private void initMiStat() {
+        MiStatInterface.initialize(this, "2882303761517926759", "5781792648759", "mi");
     }
 }
